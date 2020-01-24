7b6547b9bbc9168d3a5d4fbce82ec91f45541177
Navid <navidsafaeian@gmail.com>
2020-01-24 01:55:35 -0600

    Adding search item to user profile API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 ''' TokenAuthentication is a random token string when the user login and then every request the user makes to that API which need to authenticate, the token string is added to the request and that's effectively works as a password to check that every request made is authenticated correctly'''
+from rest_framework import filters
 
 from profiles_api import serializers 
 from profiles_api import models
@@ -97,6 +98,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
 
 
     
